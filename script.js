99a014cbb6485bf705bfbdcb56592bc4e2160cea
Stanislav Mankovskyi <odnorazka25@gmail.com>
2023-03-29 15:28:35 +0100

    mobMenu

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const themeBtn = document.querySelector(".theme-btn");
+const hamburgerMenu = document.querySelector(".hamburger");
+const mobileNavMenu = document.querySelector(".nav__list");
 
-themeBtn.addEventListener("click", themeToggle);
+themeBtn.addEventListener("click", onThemeToggle);
 
-function themeToggle() {
+hamburgerMenu.addEventListener("click", onClickHamburgerMenu);
+
+mobileNavMenu.addEventListener("click", onShowMobileMenu);
+
+function onThemeToggle() {
   themeBtn.classList.toggle("theme-btn--active");
+}
+
+function onClickHamburgerMenu() {
+  hamburgerMenu.classList.toggle("hamburger--active");
+  mobileNavMenu.classList.toggle("nav__list--active");
 }
