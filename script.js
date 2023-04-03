f769a0e90eaea7babf1d2a297ab0bbf9f702be2f
Stanislav Mankovskyi <odnorazka25@gmail.com>
2023-04-03 22:49:04 +0100

    dark-theme

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const themeBtn = document.querySelector(".theme-btn");
 const hamburgerMenu = document.querySelector(".hamburger");
 const mobileNavMenu = document.querySelector(".nav__list");
+const bodyDarkTheme = document.querySelector("body");
 
 themeBtn.addEventListener("click", onThemeToggle);
 
@@ -8,6 +9,7 @@ hamburgerMenu.addEventListener("click", onClickHamburgerMenu);
 
 function onThemeToggle() {
   themeBtn.classList.toggle("theme-btn--active");
+  bodyDarkTheme.classList.toggle("dark-theme");
 }
 
 function onClickHamburgerMenu() {
